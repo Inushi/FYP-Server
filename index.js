adf8a11afaf83e3c14053b3f7c6cf066d35248d2
Inushi <62785480+Inushi@users.noreply.github.com>
2023-06-17 14:18:10 +0530

    Hadard level

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const io = new Server(server)
 
 const PORT = process.env.PORT || 3700;
 
+const signalLvel = -150;
+
 io.on("connection", (socket) => {
     socket.on("data-retrieve", (data) => {
         console.log(data);
         io.emit("data-retrieve", data);
+
+        if(signalLvel < -120){
+            let obj = JSON.parse(data)
+            // if()
+        }
+        
     });
 
     socket.on("disconnect", () => {
